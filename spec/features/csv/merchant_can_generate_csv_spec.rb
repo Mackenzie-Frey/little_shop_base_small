b3289c54b493a7ad396927e6d108b71c3b5a531c
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-01-02 22:03:48 -0700

    Adds testing for existance of Download CSV buttons.

---
+++ spec/features/csv/merchant_can_generate_csv_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+describe 'As a Merchant visiting the dashboard' do
+  it 'sees the buttons to generate the files' do
+
+    visit dashboard_path
+
+    within "#existing-users-with-orders-from-merchant" do
+      expect(page).to have_button('Download CSV')
+    end
+    within "#new-users-without-orders-from-merchant" do
+      expect(page).to have_button('Download CSV')
+    end
+  end
+end
+
+#
+# Downloadable Merchant User Lists
+# Merchants can generate a list of email addresses for ALL EXISTING USERS who are NOT DISABLED who have
+# ORDERED ITEMS from THIS MERCHANT in the past.
+# The 4 columns must include: their name, email address,
+# and how much money they've spent on your items,
+# and how much they've spent from all merchants.
+#
+# Merchants can generate a list of all NEW USERS who have NEVER ORDERED from them before. Columns must
+# include their name, email address, how much they've spent from other merchants,
+# and how many orders they've made on the system.
+#
+# These user lists should be downloadable CSV files, one user per line in the CSV.
