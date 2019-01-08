6ea9f635b6371c8a0b65c643ad03aaee0cfbc082
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-01-08 16:30:46 -0700

    Adds test data. Adds self.top_merchants_fulfilling_non_cancelled_orders_this_month(limit) method, not yet tested.

--- app/models/user.rb
+++ app/models/user.rb
@@ -186,6 +186,15 @@ class User < ApplicationRecord
   end
 
   def self.top_merchants_fulfilling_non_cancelled_orders_this_month(limit)
+    User
+    .joins(:items, {items: :order_items})
+    .joins('INNER JOIN orders ON order_items.order_id = orders.id')
+    .select('users.*, sum(order_items.quantity) as items_sold').group(:id)
+    .where('order_items.fulfilled = ?', true)
+    .where.not('orders.status = ?', 2)
+    .where('EXTRACT(MONTH FROM order_items.updated_at) = ?', Time.now.month)
+    .order('items_sold DESC')
+    .limit(limit)
   end
 
   def self.top_merchants_fulfilling_non_cancelled_orders_last_month(limit)
--- spec/models/merchant_stats_extension_spec.rb
+++ spec/models/merchant_stats_extension_spec.rb
@@ -28,6 +28,24 @@ describe 'Merchant Index Page' do
       @order_4_b = create(:completed_order, user: @user_4)
       @order_4_c = create(:completed_order, user: @user_4)
 
+      @order_cancelled_1 = create(:cancelled_order, user: @user_1)
+      @order_cancelled_2 = create(:cancelled_order, user: @user_1)
+      @order_cancelled_3 = create(:cancelled_order, user: @user_1)
+      @order_cancelled_4 = create(:cancelled_order, user: @user_1)
+      @order_cancelled_5 = create(:cancelled_order, user: @user_1)
+      @order_cancelled_6 = create(:cancelled_order, user: @user_1)
+      @order_cancelled_7 = create(:cancelled_order, user: @user_1)
+      @order_cancelled_8 = create(:cancelled_order, user: @user_1)
+
+      @order_cancelled_9 = create(:cancelled_order, user: @user_1)
+      @order_cancelled_10 = create(:cancelled_order, user: @user_1)
+      @order_cancelled_11 = create(:cancelled_order, user: @user_1)
+      @order_cancelled_12 = create(:cancelled_order, user: @user_1)
+      @order_cancelled_13 = create(:cancelled_order, user: @user_1)
+      @order_cancelled_14 = create(:cancelled_order, user: @user_1)
+      @order_cancelled_15 = create(:cancelled_order, user: @user_1)
+      @order_cancelled_16 = create(:cancelled_order, user: @user_1)
+
       @oi_1_a = create(:fulfilled_order_item, item: @item_1, order: @order_1_a, quantity: 10, price: 10, created_at: 3.months.ago, updated_at: 2.months.ago)
       @oi_1_b = create(:fulfilled_order_item, item: @item_2, order: @order_1_b, quantity: 10, price: 10, created_at: 29.days.ago, updated_at: 27.days.ago)
 
@@ -53,6 +71,24 @@ describe 'Merchant Index Page' do
       @oi_4_b_5 = create(:fulfilled_order_item, item: @item_2, order: @order_4_b, quantity: 10, price: 10, created_at: 2.months.ago, updated_at: 1.months.ago)
       @oi_4_b_6 = create(:fulfilled_order_item, item: @item_1, order: @order_4_b, quantity: 10, price: 10, created_at: 2.months.ago, updated_at: 1.months.ago)
 
+      @oi_cancelled_order_1 = create(:fulfilled_order_item, item: @item_1, order: @order_cancelled_1, quantity: 10, price: 10, created_at: 2.minutes.ago, updated_at: 1.minutes.ago)
+      @oi_cancelled_order_2 = create(:fulfilled_order_item, item: @item_1, order: @order_cancelled_2, quantity: 10, price: 10, created_at: 2.minutes.ago, updated_at: 1.minutes.ago)
+      @oi_cancelled_order_3 = create(:fulfilled_order_item, item: @item_1, order: @order_cancelled_3, quantity: 10, price: 10, created_at: 2.minutes.ago, updated_at: 1.minutes.ago)
+      @oi_cancelled_order_4 = create(:fulfilled_order_item, item: @item_1, order: @order_cancelled_4, quantity: 10, price: 10, created_at: 2.minutes.ago, updated_at: 1.minutes.ago)
+      @oi_cancelled_order_5 = create(:fulfilled_order_item, item: @item_1, order: @order_cancelled_5, quantity: 10, price: 10, created_at: 2.minutes.ago, updated_at: 1.minutes.ago)
+      @oi_cancelled_order_6 = create(:fulfilled_order_item, item: @item_1, order: @order_cancelled_6, quantity: 10, price: 10, created_at: 2.minutes.ago, updated_at: 1.minutes.ago)
+      @oi_cancelled_order_7 = create(:fulfilled_order_item, item: @item_1, order: @order_cancelled_7, quantity: 10, price: 10, created_at: 2.minutes.ago, updated_at: 1.minutes.ago)
+      @oi_cancelled_order_8 = create(:fulfilled_order_item, item: @item_1, order: @order_cancelled_8, quantity: 10, price: 10, created_at: 2.minutes.ago, updated_at: 1.minutes.ago)
+
+      @oi_cancelled_order_9  = create(:fulfilled_order_item, item: @item_1, order: @order_cancelled_1, quantity: 10, price: 10, created_at: 2.months.ago, updated_at: 1.months.ago)
+      @oi_cancelled_order_10 = create(:fulfilled_order_item, item: @item_1, order: @order_cancelled_2, quantity: 10, price: 10, created_at: 2.months.ago, updated_at: 1.months.ago)
+      @oi_cancelled_order_11 = create(:fulfilled_order_item, item: @item_1, order: @order_cancelled_3, quantity: 10, price: 10, created_at: 2.months.ago, updated_at: 1.months.ago)
+      @oi_cancelled_order_12 = create(:fulfilled_order_item, item: @item_1, order: @order_cancelled_4, quantity: 10, price: 10, created_at: 2.months.ago, updated_at: 1.months.ago)
+      @oi_cancelled_order_13 = create(:fulfilled_order_item, item: @item_1, order: @order_cancelled_5, quantity: 10, price: 10, created_at: 2.months.ago, updated_at: 1.months.ago)
+      @oi_cancelled_order_14 = create(:fulfilled_order_item, item: @item_1, order: @order_cancelled_6, quantity: 10, price: 10, created_at: 2.months.ago, updated_at: 1.months.ago)
+      @oi_cancelled_order_15 = create(:fulfilled_order_item, item: @item_1, order: @order_cancelled_7, quantity: 10, price: 10, created_at: 2.months.ago, updated_at: 1.months.ago)
+      @oi_cancelled_order_16 = create(:fulfilled_order_item, item: @item_1, order: @order_cancelled_8, quantity: 10, price: 10, created_at: 2.months.ago, updated_at: 1.months.ago)
+
     end
       describe 'class methods' do
         it '.top_merchants_selling_items_this_month(limit)' do
@@ -67,18 +103,14 @@ describe 'Merchant Index Page' do
           expect(User.top_merchants_selling_items_last_month(3)[0].items_sold).to eq(40)
         end
 
-        xit '.top_merchants_fulfilling_non_cancelled_orders_this_month(limit)' do
+        it '.top_merchants_fulfilling_non_cancelled_orders_this_month(limit)' do
 
-          expect(User.top_merchants_selling_items_last_month(3)).to eq([@merchant_, @merchant_, @merchant_])
-          expect(User.top_merchants_selling_items_last_month(3)[0]).to eq(@merchant_)
-          expect(User.top_merchants_selling_items_last_month(3)[1]).to eq(@merchant_)
+          expect(User.top_merchants_fulfilling_non_cancelled_orders_this_month(3)).to eq([@merchant_4, @merchant_3, @merchant_2])
         end
 
         xit '.top_merchants_fulfilling_non_cancelled_orders_last_month(limit)' do
 
-          expect(User.top_merchants_fulfilling_non_cancelled_orders_last_month(3)).to eq([@merchant_, @merchant_, @merchant_])
-          expect(User.top_merchants_fulfilling_non_cancelled_orders_last_month(3)[0]).to eq(@merchant_)
-          expect(User.top_merchants_fulfilling_non_cancelled_orders_last_month(3)[1]).to eq(@merchant_)
+          expect(User.top_merchants_fulfilling_non_cancelled_orders_last_month(3)).to eq([@merchant_3, @merchant_2, @merchant_1])
         end
 
         xit '.top_merchants_fulfilling_fastest_orders_my_state(limit)' do
@@ -98,36 +130,6 @@ describe 'Merchant Index Page' do
   end
   end
 
-
-# Step 1:
-# Think about what each stat is looking for and get all of tests written with all of the test data.
-
-# Ex. in controller
-	# @top_10_merchants_items_sold_this_month = User.top_merchants_items_sold_this_month(10)
-  #   @top_10_merchants_items_sold_last_month = User.top_merchants_items_sold_last_month(10)
-  #   @top_10_merchants_fulfilled_orders_this_month = User.top_merchants_fulfilled_orders_this_month(10)
-  #   @top_10_merchants_fulfilled_orders_last_month = User.top_merchants_fulfilled_orders_last_month(10)
-
-# Step 2:
-# Start with Model Test
-# Go line by line to see what I have
-
-# ________________________________________________________________________________
-# Build a Merchant leaderboard available on "/merchants" that all users can see:
-# ________________________________________________________________________________
-
-# 1. Top 10 Merchants who sold the most items this month
-# -----------------------------------------------------
-# Merchants who sold the most items in a month:
-# this means the order_item was fulfilled in that month,
-# and the order status is completed. “Items” mean units sold,
-# a sum of quantities within the merchant’s order_items.
-# “Month” is a calendar month. So right now in January,
-# there would only be three days to look at.
-
-# 2. Top 10 Merchants who sold the most items last month
-# ------------------------------------------------------
-
 # 3. Top 10 Merchants who fulfilled non-cancelled orders this month
 # -----------------------------------------------------------
 # Merchants who fulfilled non-cancelled orders:
@@ -140,7 +142,7 @@ describe 'Merchant Index Page' do
 # 4. Top 10 Merchants who fulfilled non-cancelled orders last month
 # ----------------------------------------------------------
 
-# When logged in as a user:
+# WHEN LOGGED IN AS ONLY A USER !!!!!!!!!!!
 # __________________________________________________________________________________
 # 5. Also see top 5 merchants who have fulfilled items the fastest to my state
 # -----------------------------------------------------------
