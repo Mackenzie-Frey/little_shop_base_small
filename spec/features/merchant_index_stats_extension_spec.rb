5dc3cd671e271506129829b0591df439844f96fd
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-01-07 20:15:24 -0700

    Adds comments for action plan.

--- spec/features/merchant_index_stats_extension_spec.rb
+++ spec/features/merchant_index_stats_extension_spec.rb
@@ -9,11 +9,32 @@ RSpec.describe 'Merchant Index Page', type: :feature do
 end
 
 
+# Step 1:
+# Think about what each stat is looking for and get all of tests written with all of the test data.
 
+# Model Tests:
+# Pass in a count of how many things trying to output.
+# To test for 3 things instead of 10
+# def self.top_merchants_items_sold_this_month(count)
+
+# Ex. in controller
+	# @top_10_merchants_items_sold_this_month = User.top_merchants_items_sold_this_month(10)
+  #   @top_10_merchants_items_sold_last_month = User.top_merchants_items_sold_last_month(10)
+  #   @top_10_merchants_fulfilled_orders_this_month = User.top_merchants_fulfilled_orders_this_month(10)
+  #   @top_10_merchants_fulfilled_orders_last_month = User.top_merchants_fulfilled_orders_last_month(10)
+
+# Step 2:
+# Go line by line to see what I have
+# Can use Ian's code as a launching spot
+# Might need to join all 4 tables (might not be a standard joins)
+
+
+
+# ________________________________________________________________________________
 # Build a Merchant leaderboard available on "/merchants" that all users can see:
 # ________________________________________________________________________________
 
-# Top 10 Merchants who sold the most items this month
+# 1.  Top 10 Merchants who sold the most items this month
 # -----------------------------------------------------
 # Merchants who sold the most items in a month:
 # this means the order_item was fulfilled in that month,
@@ -23,10 +44,10 @@ end
 # there would only be three days to look at.
 
 
-# Top 10 Merchants who sold the most items last month
+# 2. Top 10 Merchants who sold the most items last month
 # ------------------------------------------------------
 
-# Top 10 Merchants who fulfilled non-cancelled orders this month
+# 3. Top 10 Merchants who fulfilled non-cancelled orders this month
 # -----------------------------------------------------------
 # Merchants who fulfilled non-cancelled orders:
 # this means the merchants fulfilled their items AND
@@ -35,13 +56,13 @@ end
 # un-fulfilled items in any order then that will satisfy
 # everything
 
-# Top 10 Merchants who fulfilled non-cancelled orders last month
+# 4. Top 10 Merchants who fulfilled non-cancelled orders last month
 # ----------------------------------------------------------
 
 # When logged in as a user:
 # __________________________________________________________________________________
-# Also see top 5 merchants who have fulfilled items the fastest to my state
+# 5. Also see top 5 merchants who have fulfilled items the fastest to my state
 # -----------------------------------------------------------
 
-# Also see top 5 merchants who have fulfilled items the fastest to my city
+# 6. Also see top 5 merchants who have fulfilled items the fastest to my city
 # -----------------------------------------------------------
